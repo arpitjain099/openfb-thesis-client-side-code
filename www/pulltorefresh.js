b75c079704a94bdb3f73953d7829e3f20fd9df64
Arpit Jain <arpitjain099@gmail.com>
2015-06-11 11:18:21 +1300

    code before mock1

---
+++ www/pulltorefresh.js
@@ -0,0 +1,14 @@
+/*!
+* jquery.plugin.pullToRefresh.js
+* version 1.0
+* author: Damien Antipa
+* https://github.com/dantipa/pull-to-refresh-js
+*/
+(function( $ ){
+
+	$.fn.pullToRefresh = function( options ) {
+
+	window.location.reload();
+
+	};
+})( jQuery );
